d5691fe8235de1fad7c3f38cad5a7e8ede3961cc
Ronish <ronishpanjwani@gmail.com>
2019-03-06 22:06:51 -0800

    first commit:wq :

---
+++ src/com/arborsresi/training/Hello.java
@@ -0,0 +1,17 @@
+package com.arborsresi.training;
+
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		Hello h = new Hello();
+		h.getName("Ronish");
+		
+	}
+	public String getName(String name)
+	{
+		return name;
+	}
+
+}
+
---
+++ src/com/arborsresi/training/Hellotest.java
@@ -0,0 +1,6 @@
+package com.arborsresi.training;
+
+public class Hellotest
+{
+
+}
